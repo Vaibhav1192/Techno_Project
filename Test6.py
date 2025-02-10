21517899f635d7f8b0ab31e3efe7a18ade4ffaf1
vaibhav1192 <vbhvkdng95@gmail.com>
2025-02-10 13:23:22 +0530

    Third Commit

--- Test6.py
+++ Test6.py
@@ -1,3 +1,4 @@
+# Try.....Except
 try:
     a = 10
     b = 0
@@ -5,3 +6,26 @@ try:
 except ZeroDivisionError:
     # Code that runs if a ZeroDivisionError occurs
     print("Cannot divide by zero")
+
+finally:
+    # Code that always runs, regardless of exceptions
+    print("This will always execute")
+
+# Try....Finally
+try:
+    a = 10
+    b = 0
+    result = a / b
+finally:
+    # Code that always runs, regardless of exceptions
+    print("This will always execute")
+
+
+def foo():
+    try:
+        return 1
+    
+    finally:
+        return 2
+K = foo()
+print(K)
